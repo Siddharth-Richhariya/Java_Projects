bdff5c0eb11e8a2480e0e1e8e9db63920154f9a9
Siddharth Richhariya <siddharth.richhariya123@gmail.com>
2024-04-13 13:12:36 +0530

    Right Angle Star Pattern

---
+++ src/JavaBasicCodes/RightAngleStarTriangle.java
@@ -0,0 +1,13 @@
+package JavaBasicCodes;
+
+public class RightAngleStarTriangle {
+    public static void main(String[] args) {
+        int num = 6;
+        for(int i= 1;i <=num; i++ ){
+            for (int j = 1; j <=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
