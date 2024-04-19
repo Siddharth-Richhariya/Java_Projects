e929bf5ea33dd57adf4b1c715389647e6c6039af
Siddharth Richhariya <siddharth.richhariya123@gmail.com>
2024-04-19 18:44:18 +0530

    Found Duplicates in Array

---
+++ src/JavaBasicCodes/DuplicatesArrayIndexValue.java
@@ -0,0 +1,23 @@
+package JavaBasicCodes;
+
+
+public class DuplicatesArrayIndexValue {
+    public static void  Duplicate(int []arr){
+        boolean foundDuplicate = false;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println("Duplicate value: " + arr[i]);
+                    foundDuplicate = true;
+                }
+            }
+        }
+        if (!foundDuplicate) {
+            System.out.println("No Duplicate Values");
+        }
+    }
+    public static void main(String[] args) {
+        int []arr = {3,5,6,2,6,3,7,8,0};
+        Duplicate(arr);
+    }
+}
