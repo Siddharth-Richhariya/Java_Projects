2bec32f47f6d46c42a5faeede6947db4f5286054
Siddharth Richhariya <siddharth.richhariya123@gmail.com>
2024-04-20 23:07:14 +0530

    Swap Numbers

---
+++ src/JavaBasicCodes/SwapNumbers.java
@@ -0,0 +1,13 @@
+package JavaBasicCodes;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 7;
+        int c = b;
+         b = a;
+         a =c;
+        System.out.println("Value of A :"+ a);
+        System.out.println("Value of B :"+ b);
+    }
+}
