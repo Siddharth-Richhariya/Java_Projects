5d88e671a5c9a7635a2c70b21cb55725348dec9e
Siddharth Richhariya <siddharth.richhariya123@gmail.com>
2024-04-21 21:54:13 +0530

    Fibonacci Series

---
+++ src/JavaBasicCodes/FibonacciSeries.java
@@ -0,0 +1,19 @@
+package JavaBasicCodes;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        FSeries(8);
+    }
+    public static void FSeries(int num){
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        for(int i =1; i <= num; i++){
+            System.out.print(a+ ", ");
+            a = b;
+            b = c;
+            c = a +b;
+
+        }
+    }
+}
