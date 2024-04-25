86fa5f03e43be2e83e54393c3887a7933f4d012c
Siddharth Richhariya <siddharth.richhariya123@gmail.com>
2024-04-26 02:05:21 +0530

    Prime Number Checker

--- src/JavaBasicCodes/PrimeNumberChecker.java
+++ src/JavaBasicCodes/PrimeNumberChecker.java
@@ -3,11 +3,9 @@ package JavaBasicCodes;
 import java.util.Scanner;
 
 public class PrimeNumberChecker {
-    public Boolean PrimeNumber(int num){
-        if (num == 1){
+    public static Boolean PrimeNumber(int num){
+        if (num == 1 || num < 1){
             return true;
-        } else if ((num < 1)) {
-            return false;
         }
         for (int i = 2; i < num; i++){
             if (num % i == 0){
@@ -20,14 +18,16 @@ public class PrimeNumberChecker {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Number : ");
         int num = sc.nextInt();
-        PrimeNumberChecker pn = new PrimeNumberChecker();
-        boolean isPrime = pn.PrimeNumber(num);
+        sc.close();
+
+        boolean isPrime = PrimeNumber(num);
         if(isPrime){
             System.out.println("Prime Number ");
         }
         else {
             System.out.println("Not a Prime Number ");
         }
+
 
 
     }
