8c7ad4cd86cb9dea2f358af77764d88bced88a7d
Siddharth-Richhariya <siddharth.richhariy123@gmail.com>
2024-05-16 19:08:55 +0530

    Odd even Placed charter in Array

---
+++ src/ArraysAndStringProgramms/OddEvenPlacedCharactersInAString.java
@@ -0,0 +1,28 @@
+package ArraysAndStringProgramms;
+
+import java.util.Scanner;
+
+public class OddEvenPlacedCharactersInAString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String : ");
+        String str = sc.next();
+        selectStringByPosition(str);
+    }
+    public static void selectStringByPosition (String str) {
+        StringBuilder oddChar = new StringBuilder();
+        StringBuilder evenChar = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            char character = str.charAt(i);
+            if (i % 2 == 0) {
+                oddChar.append(character);
+            } else {
+                evenChar.append(character);
+            }
+        }
+        System.out.println("Odd Placed Charcter : "+oddChar);
+        System.out.println("Even Placed Charcter : "+evenChar);
+
+    }
+}
