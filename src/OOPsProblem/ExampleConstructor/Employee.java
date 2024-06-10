9944576f328077c34e4723b298f1dda280095d99
Siddharth Richhariya <siddharth.richhariya123@gmail.com>
2024-06-10 23:10:33 +0530

    Constuctor Example

---
+++ src/OOPsProblem/ExampleConstructor/Employee.java
@@ -0,0 +1,30 @@
+package OOPsProblem.ExampleConstructor;
+
+public class Employee {
+    String name;
+    String companyName;;
+    int empID;
+    double salary;
+    String BU;
+    String designation;
+
+    Employee(){
+        System.out.println("This is avenger");
+    }
+
+    Employee(String Name, int empID, String companyname,double salary, String BU, String Designation){
+        this.name = Name;
+        this.empID = empID;
+        this.salary = salary;
+        this.BU = BU;
+        this.designation = Designation;
+        this.companyName = companyname;
+
+        System.out.println("Employee name ->" +name);
+        System.out.println("Employee ID ->" +empID);
+        System.out.println("Employee Company Name ->" +companyname);
+        System.out.println("Employee salary ->" +salary);
+        System.out.println("Employee BU ->" +BU);
+        System.out.println("Employee Designation ->" +designation);
+    }
+}
---
+++ src/OOPsProblem/ExampleConstructor/Main.java
@@ -0,0 +1,9 @@
+package OOPsProblem.ExampleConstructor;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee emp = new Employee();
+        Employee emp1 = new Employee("Peter", 543,"Stark Industries", 120000, "Companion", "Avenger");
+
+    }
+}
