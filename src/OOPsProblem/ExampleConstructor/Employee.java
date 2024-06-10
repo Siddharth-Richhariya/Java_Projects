d7cff2d6fb427af490eab12d0e1179398c2663f8
Siddharth Richhariya <siddharth.richhariya123@gmail.com>
2024-06-10 23:20:42 +0530

    Constuctor Example

--- src/OOPsProblem/ExampleConstructor/Employee.java
+++ src/OOPsProblem/ExampleConstructor/Employee.java
@@ -8,9 +8,6 @@ public class Employee {
     String BU;
     String designation;
 
-    Employee(){
-        System.out.println("This is avenger");
-    }
 
     Employee(String Name, int empID, String companyname,double salary, String BU, String Designation){
         this.name = Name;
@@ -20,9 +17,13 @@ public class Employee {
         this.designation = Designation;
         this.companyName = companyname;
 
+    }
+
+    void printDetails ()
+    {
         System.out.println("Employee name ->" +name);
         System.out.println("Employee ID ->" +empID);
-        System.out.println("Employee Company Name ->" +companyname);
+        System.out.println("Employee Company Name ->" +companyName);
         System.out.println("Employee salary ->" +salary);
         System.out.println("Employee BU ->" +BU);
         System.out.println("Employee Designation ->" +designation);
--- src/OOPsProblem/ExampleConstructor/Main.java
+++ src/OOPsProblem/ExampleConstructor/Main.java
@@ -2,8 +2,8 @@ package OOPsProblem.ExampleConstructor;
 
 public class Main {
     public static void main(String[] args) {
-        Employee emp = new Employee();
-        Employee emp1 = new Employee("Peter", 543,"Stark Industries", 120000, "Companion", "Avenger");
 
+        Employee emp1 = new Employee("Peter", 543,"Stark Industries", 120000, "PowerFull & Flexible", "Avenger");
+        emp1.printDetails();
     }
 }
