8a2bf91682d1b2f3bbb024d4416b1aada8f0b425
Siddharth Richhariya <siddharth.richhariya123@gmail.com>
2024-06-12 00:33:14 +0530

    Inheritance Example

--- src/OOPsProblem/ExampleInheritance/hierarchicalInheritance/Main.java
+++ src/OOPsProblem/ExampleInheritance/hierarchicalInheritance/Main.java
@@ -3,7 +3,6 @@ package OOPsProblem.ExampleInheritance.hierarchicalInheritance;
 public class Main {
     public static void main(String[] args) {
         Child1 c1 = new Child1();
-        c1.str= "New Value";
         c1.print1();
         c1.print();
         System.out.println(c1.str);
@@ -11,7 +10,6 @@ public class Main {
         System.out.println();
 
         Child2 c2 = new Child2();
-        c2.str= "New value2";
         c2.print2();
         c2.print();
         System.out.println(c2.str);
--- src/OOPsProblem/ExampleInheritance/hierarchicalInheritance/Parent.java
+++ src/OOPsProblem/ExampleInheritance/hierarchicalInheritance/Parent.java
@@ -2,7 +2,6 @@ package OOPsProblem.ExampleInheritance.hierarchicalInheritance;
 
 public class Parent {
     String name = "Parent Class Varaible";
-    String str ;
 
     void print(){
         System.out.println("Parent Class");
