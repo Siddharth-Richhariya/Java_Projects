22b69e5c63abaa91065650847c046940080d102f
Siddharth Richhariya <siddharth.richhariya123@gmail.com>
2024-06-12 23:17:44 +0530

    Constructor Example

---
+++ src/OOPsProblem/ExampleConstructor/ConstructorChaining/Dog.java
@@ -0,0 +1,19 @@
+package OOPsProblem.ExampleConstructor.ConstructorChaining;
+
+public class Dog {
+    String type;
+    String Breed;
+
+    Dog(){
+        System.out.println("This is DOG Class");
+    }
+    Dog(String type){
+        this();
+        this.type = type;
+    }
+    Dog(String Breed, String type){
+        this(type);
+        this.Breed = Breed;
+        System.out.println("Type : "+ type + "\nBreed : "+ Breed);
+    }
+}
---
+++ src/OOPsProblem/ExampleConstructor/ConstructorChaining/Main.java
@@ -0,0 +1,7 @@
+package OOPsProblem.ExampleConstructor.ConstructorChaining;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog D1 = new Dog("Husky", "Wolf");
+    }
+}
