3f10926b1582904265f15d8d1b96184628e293e7
Siddharth Richhariya <157848011+Siddharth-Richhariya@users.noreply.github.com>
2024-06-13 23:39:38 +0530

    Update main.java

--- src/OOPsProblem/ExamplePolymorphism/Overloading/main.java
+++ src/OOPsProblem/ExamplePolymorphism/Overloading/main.java
@@ -2,10 +2,10 @@ package OOPsProblem.ExamplePolymorphism.Overloading;
 
 public class main extends BaseClass{
     public static void main(String[] args) {
-        record();                                                           //No Input
-        record("John");                                             //John
-        record(01, "Alex");                                   //Roll No . - 1, Name - Alex
-        record(02, "Bob", 'C');                         //Roll No . - 2, Name - Bob, Grade - C
-        record(03, "Charlie",'A',92);         //Roll No . - 3, Name - Charlie, Grade - A, Attandance - 92.0%
+        record();                              //No Input
+        record("John");                        //John
+        record(01, "Alex");                    //Roll No . - 1, Name - Alex
+        record(02, "Bob", 'C');                //Roll No . - 2, Name - Bob, Grade - C
+        record(03, "Charlie",'A',92);         //Roll No . - 3, Name - Charlie, Grade - A, Attendance - 92.0%
     }
 }
