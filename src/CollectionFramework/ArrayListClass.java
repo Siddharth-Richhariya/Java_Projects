01f845d6cc1dd5a8008865d5597a109f581a4f7e
Siddharth Richhariya <siddharth.richhariya123@gmail.com>
2024-06-18 02:15:49 +0530

    ArrayList

---
+++ src/CollectionFramework/ArrayListClass.java
@@ -0,0 +1,11 @@
+package CollectionFramework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListClass {
+    public static void main(String[] args) {
+        List myList = new ArrayList();
+        myList.add("Siddharth");
+    }
+}
